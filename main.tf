ab6aa441cbafaf7f2f579c3062e0d6b829e71b29
ssp2024 <166893249+ssp2024@users.noreply.github.com>
2024-04-13 15:10:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
+module "ec2" {
+  source = "./modules/ec2"
   ami_value = var.ami_id
   instance_type_value = var.instance_type
   subnet_id_value = var.subnet_id
