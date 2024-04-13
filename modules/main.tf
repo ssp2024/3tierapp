c8c57bd882ea905aa6a1b79e86f03eab85f1accb
ssp2024 <166893249+ssp2024@users.noreply.github.com>
2024-04-13 15:14:10 +0530

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.cidr
+}
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "sub2" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
